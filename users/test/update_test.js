30d2c377fdd5d322032ebe94c0ead4a034f5238e
Miguel Patiño <miguel36410@gmail.com>
2020-05-06 16:41:54 +0200

    Required Field to User and validateSync

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updateing records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({'name': 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
         .then(() => done());
     });
@@ -49,5 +49,14 @@ describe('Updateing records', () => {
             done
         );
     });
+
+    it('A user can have their postcount incremented by 1', (done) => {
+        User.update({ name: 'Joe'}, {$inc: {postCount: 1}})
+        .then(() => User.findOne({name: 'Joe'}))
+        .then((user) => {
+            assert(user.postCount === 1);
+            done();
+        });
+    })
 });
 
