41ed79d1e6668ce6c7bc4902d9dc2c5c84853095
Miguel Patiño <miguel36410@gmail.com>
2020-05-08 02:58:15 +0200

    Associations

---
+++ users/test/association_test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+describe('Associations', () => {
+    let joe, blogPost, comment;
+    beforeEach((done) => {
+        joe = new User({name: 'Joe'});
+        blogPost = new BlogPost({title: 'JS is Great', content: 'A Content'});
+        comment = new Comment({content: 'Congrants for ur posts'});
+
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
+
+        Promise.all([joe.save(), blogPost.save(), comment.save()])
+            .then(() => done());
+    });
+
+    it.only('saves a  realation between a user and a blogpost', (done) => {
+        User.findOne({ name: 'Joe' })
+        .populate('blogPosts')
+        .then((user) => {
+            assert(user.blogPosts[0].title === 'JS is Great');
+            done();
+        });
+    });
+});
+
