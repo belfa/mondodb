c0d3e6b2e2f346d1a967aef4afe756ce3475f430
Miguel Patiño <miguel36410@gmail.com>
2020-05-17 02:10:44 +0200

    Basic structure

---
+++ muber/routes/routes.js
@@ -0,0 +1,7 @@
+const DirversController = require('../controllers/drivers_controllers');
+
+module.exports = (app) => {
+    // Watch for incoming requestss of method GET
+    // to the reoute http://localhost:3050/api
+    app.get('/api', DirversController.greeting);
+}
